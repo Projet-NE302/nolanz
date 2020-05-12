4dc7b7a11b846799af5decf4365e53d9d4d0850d
Nolan-Zer <64536636+Nolan-Zer@users.noreply.github.com>
2020-05-12 15:58:36 +0200

    d

--- etape3NE302/methodCheck.c
+++ etape3NE302/methodCheck.c
@@ -20,16 +20,18 @@ int methodCheck(){
   if(s) nodes=(Lnode*)s->node;
   if(t) nodet=(Lnode*)t->node;
 
-  if(e) {
+  if(e && s) {
     while(e->next) e=e->next;
     nodee=(Lnode*)e->node;
     if(nodee->value!=NULL)if(!strcmp(nodee->value,"chunked")) check = -3; // Conformity of transfer_encoding value.
   }
-  printf("sss\n");
+
   if(!(strcmp(node->value,"GET")) && !(strcmp(node->value,"HEAD")) && !(strcmp(node->value,"POST"))) check = -1; // Conformity of method value.
-  printf("sss\n");
-  while(nodes->value[size] != '\r') size++;
-  if(size != atoi(nodet->value)) check = -2; // Conformity size of message_body compared to Content length value.
+
+  if(s && t){
+    while(nodes->value[size] != '\r') size++;
+    if(size != atoi(nodet->value)) check = -2; // Conformity size of message_body compared to Content length value.
+  }
 
   if(check==-1){
     printf("Method non-compliant.\n");

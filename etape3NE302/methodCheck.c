dda16eae024e0711b43fcf225eef5b9ac71661e9
Nolan-Zer <64536636+Nolan-Zer@users.noreply.github.com>
2020-05-12 15:47:31 +0200

    d

--- etape3NE302/methodCheck.c
+++ etape3NE302/methodCheck.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-##include <string.h>
+#include <string.h>
 #include "httpparser.h"
 #include "api.h"
 
@@ -12,23 +12,22 @@ int methodCheck(){
   r=searchTree(root,"method");
   s=searchTree(root,"message_body");
   e=searchTree(root,"transfer_encoding");
-  t=searchTree(root,"content-length");
-  print("bbnb\n");
+  t=searchTree(root,"content_length");
+  printf("bbnb\n");
   Lnode *node, *nodes, *nodet, *nodee;
   int size = 0, i=0;
   node=(Lnode*)r->node;
   nodes=(Lnode*)s->node;
   nodet=(Lnode*)t->node;
-  nodee=(Lnode*)e->node;
 
+  while(e->next) e=e->next;
+  nodee=(Lnode*)e->node;
+  if(nodee->value!=NULL)if(!strcmp(nodee->value,"chunked")) check = -3; // Conformity of transfer_encoding value.
+  
   if(!(strcmp(node->value,"GET")) && !(strcmp(node->value,"HEAD")) && !(strcmp(node->value,"POST"))) check = -1; // Conformity of method value.
 
   while(nodes->value[i] != '\r') size++;
   if(size != atoi(nodet->value)) check = -2; // Conformity size of message_body compared to Content length value.
-  if(nodee->value != NULL){
-    while(nodee->next != NULL) nodee=nodee->next;
-    if(!strcmp(nodee->value,"chunked")) check = -3; // Conformity of transfer_encoding value.
-  }
 
   if(check==-1){
     printf("Method non-compliant.\n");

59977affa6852a84b8163ea25dfefbaca95caa53
Nolan-Zer <64536636+Nolan-Zer@users.noreply.github.com>
2020-05-12 15:26:02 +0200

    d

---
+++ etape3NE302/methodCheck.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "httpparser.h"
+#include "api.h"
+
+int methodCheck(){
+  _Token *root, *r, *s, *e, *t;
+  int check = 1;
+  root=getRootTree();
+  r=searchTree(root,"method");
+  s=searchTree(root,"message_body");
+  e=searchTree(root,"transfer_encoding");
+  t=searchTree(root,"content-length");
+
+  Lnode *node, *nodes, *nodet;
+  int size = 0;
+  node=(Lnode*)r->node;
+  nodes=(Lnode*)s->node;
+  nodet=(Lnode*)t->node;
+  nodee=(Lnode*)e->node;
+
+  if(!(strcmp(node->value,"GET")) && !(strcmp(node->value,"HEAD")) && !(strcmp(node->value,"POST"))) check = -1; // Conformity of method value.
+
+  while(nodes->value[i] != '\r') size++;
+  if(size != atoi(nodet->value)) check = -2; // Conformity size of message_body compared to Content length value.
+  if(nodee->value != NULL){
+    while(nodee->next != NULL) nodee=nodee->next;
+    if(!strcmp(nodee->value,"chunked")) check = -3; // Conformity of transfer_encoding value.
+  }
+
+  if(check==-1){
+    printf("Method non-compliant.\n");
+    return check;
+  }
+  if(check==-2){
+    printf("Content-length non-compliant.\n");
+    return check;
+  }
+  if(check==-3){
+    printf("Transfer-encoding non-compliant.\n");
+    return check;
+  }
+  if(check==1){
+    printf("[Method & Message-body CHECK] : OK.\n");
+    return check;
+  }
+}
